f904e2b7717be72b76597dd776f21abda2bb5088
Akuna Joshua <akunajoshua@gmail.com>
2023-02-11 22:19:06 +0100

    Canvas and Brick class initial commit.

--- Brick.java
+++ Brick.java
@@ -1,5 +1,22 @@
+import java.awt.*;
+
 public class Brick extends BaseObject{
     public Brick(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Brick (double x, double y){
+        this(x, y, 3);
+    }
+    //TASK 12
+    // overrides the move method.
+    @Override
+    void move() {
+        // This method does nothing because the Brick
+        // instances does not move.
+    }
+    // overrides and implements the draw method.
+    @Override
+    void draw(Canvas canvas) {
     }
 }
--- Canvas.java
+++ Canvas.java
@@ -1,4 +1,7 @@
+import java.awt.*;
+
 /**
+ * TASK 9
  * The Canvas class will define an instance on which
  * all the objects will be drawn.
  */
@@ -40,5 +43,48 @@ public class Canvas {
 
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
+    }
+
+    /*
+     * TASK 10
+     * Implement the
+     * 1. void setPoint (double x, double y, char c)
+     * 2. void drawMatrix (double x, double y, int[][] matrix, char c)
+     */
+    public void setPoint (double x, double y, char c){
+        // sets the element of index x and y in the matrix to c.
+        // if x and y is within the range of the canvas width and height.
+        if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
+            matrix[(int)Math.round(x)][(int)Math.round(y)] = c;
+        }
+    }
+
+    public void drawMatrix (double x, double y, int[][] matrixParam, char c){
+        // Copies the matrix argument to the Canvas matrix starting at
+        // coordinate(x, y). Using nested loops paint the value of
+        // matrix[x+j][y+i] as c if matrixParam[i][j] is not equal to 0.
+        // Using setPoint(x + j, y + i)
+
+        for (int i = 0; i < matrixParam.length; i++){
+            for (int j = 0; j < matrixParam[0].length; j++){
+                if(matrixParam[i][j] != 0) setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+    // TASK 11: add the clear() and print()
+    public void clear(){
+        // clears the matrix
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public void print(){
+        // prints the matrix
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++){
+                System.out.printf("%c\t", matrix[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
