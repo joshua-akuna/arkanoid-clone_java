eec8d748db67e3f2efff54c96d652e3d1969d0e1
Akuna Joshua <akunajoshua@gmail.com>
2023-02-12 10:40:30 +0100

    Paddle class initial boilerplate codes

--- Paddle.java
+++ Paddle.java
@@ -1,6 +1,26 @@
+/**
+ * TASK 16: Setting up the paddle class
+ * add the following fields
+ * 1. double speed (paddle speed)
+ * 2. direction (moves only in the x-axis.
+ *  a. value 1: to the right
+ *  b. value -1: to the left
+ *  c. value 0: no motion, initial value.
+ *  d. initialize the class with a consructor
+ *  e. add getters for the field.
+ */
 public class Paddle extends BaseObject{
-    public Paddle(double x, double y, double radius) {
+    private double speed;
+    private double direction;
+
+    public Paddle (double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Paddle (double x, double y){
+        this(x, y, 3);
+        this.speed = 1;
+        this.direction = 0;
     }
 
     @Override
@@ -10,6 +30,9 @@ public class Paddle extends BaseObject{
 
     @Override
     void draw(Canvas canvas) {
-
     }
+
+    public double getSpeed() { return speed; }
+
+    public double getDirection() { return direction;}
 }
